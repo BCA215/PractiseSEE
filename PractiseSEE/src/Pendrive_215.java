e62fd773164401e0b0158d00382a6b73ca13ae9e
abcd <abcd@abcd-PC>
2022-06-14 19:51:24 +0530

    commit1

--- PractiseSEE/src/Pendrive_215.java
+++ PractiseSEE/src/Pendrive_215.java
@@ -6,6 +6,10 @@ public class Pendrive_215 {
 	Pendrive_215(int size){
 		this.size=size;
 	}
+	int get_size()
+	{
+		return size;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
